4d1ffe7decbcda49063e399693ca5b83141470ca
Ikumarucyanman <92787585+Ikumarucyanman@users.noreply.github.com>
2021-12-02 20:07:01 +0900

    OK

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -52,7 +52,7 @@ class PrototypesController < ApplicationController
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
-
+  
   def set_prototype
     @prototype = Prototype.find(params[:id])
   end
